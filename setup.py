3be0cb69be69c19cac3e99d9388cf23b0864c14d
Eric Schles <ericschles@gmail.com>
2020-04-06 21:22:02 -0400

    type: feature files added: setup.py description: adding setup.py file and overall structure of library

---
+++ setup.py
@@ -0,0 +1,31 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="backtester",
+    version="0.1",
+    description="A backtesting for timeseries data in a pandas dataframe",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/EricSchles/backtester",
+    author="Eric Schles",
+    author_email="ericschles@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["backtester", 'backtester.metrics', 'backtester.backtest'],
+    include_package_data=True,
+    install_requires=["sklearn", "scipy", "numpy",
+                      "statsmodels", "mlxtend", "pytest"],
+)
