06595ad243e67563c4775c4c37451f0a73156f4a
Eric Schles <ericschles@gmail.com>
2020-04-08 13:28:49 -0400

    type: feature files added: none description: api documentation complete

--- backtester/metrics/metrics.py
+++ backtester/metrics/metrics.py
@@ -273,7 +273,7 @@ rmse = root_mean_squared_error
 mape = mean_absolute_percentage_error
 mbrae = mean_bounded_relative_absolute_error
 
-def ad_fuller_test(timeseries):
+def ad_fuller_test(timeseries: pd.Series):
     """
     Ad fuller documentation here:
     https://www.statsmodels.org/stable/generated/statsmodels.tsa.stattools.adfuller.html#statsmodels.tsa.stattools.adfuller
@@ -294,29 +294,147 @@ def ad_fuller_test(timeseries):
     AdFullerResult = namedtuple('AdFullerResult', 'statistic pvalue')
     return AdFullerResult(result[0], result[1])
 
-def kpss(timeseries):
+def kpss(timeseries: pd.Series, regression="c", nlags=None):
     """
+    KPSS documentation here:
+    https://www.statsmodels.org/stable/generated/statsmodels.tsa.stattools.kpss.html#statsmodels.tsa.stattools.kpss
     
+    Tests to see if the time series is trend stationary.
+    
+    Null hypothesis:
+    The time series is level or trend stationary
+    
+    Alternative hypothesis:
+    A unit root exists
+    
+    Parameters
+    ----------
+    * timeseries : pd.Series
+      The observations of the time series
+    * regression : str
+      The time of model to use.
+      If regression == 'c' check for stationarity around
+      r_0.  Aka, difference against the mean
+      If regression == 'ct' check for stationarity against
+      the trend.  Aka, run OLS and run test against residuals.
+    * nlags : str or int
+      The number of lags to consider.
+      If nlags == 'legacy' use:
+      ceiling(12 * (n_observations/100)**(1/4)
+      if nlags == 'auto' tune the number of lags
+      if nlags == int, use that as number of lags.
+    
+    Note: all values are assumed to belong to the same time index
+
+    Returns
+    -------
+    Geometric Mean Absolute Percentage Error
+
     """
-    result = kpss(timeseries)
+    result = kpss(
+        timeseries, regression=regression,
+        nlags=nlags, store=False
+    )
     KPSSResult = namedtuple('KPSSResult', 'statistic pvalue')
     return KPSSResult(result[0], result[1])
 
-def cointegration(y_true, y_pred):
+def cointegration(y_zero: pd.Series, y_one: pd.Series, trend='c', maxlag=None, autolag='aic'):
     """
+    Cointegration documentation:
+    https://www.statsmodels.org/stable/generated/statsmodels.tsa.stattools.coint.html#statsmodels.tsa.stattools.coint
+    
+    Null hypothesis:
+    No cointegration
+    
+    Assumption: y_zero and y_one are assumed to be integrated
+    of order 1, I(1).
+    
+    Alternative hypothesis:
+    cointegration exists between the two series.
+    
+    Definition: Cointegration - if two series are said to be 
+    cointegrated then there exists a statistically significant connection between them.
+    Specifically, some linear combination form a stationary time series.
+    
+    Implication: If y_zero, y_one both have a unit root, they are not stationary.
+    But if some linear combination is stationary then there exists a long running
+    relationship between the two series.
+    
+    Parameters
+    ----------
+    y_zero : pd.Series
+        The first element in cointegrated system. Must be 1-d.
+    y_one : pd.Series
+        The remaining elements in cointegrated system.
+    trend : str {'c', 'ct'}
+        The trend term included in regression for cointegrating equation.
+
+        * 'c' : constant.
+        * 'ct' : constant and linear trend.
+        * also available quadratic trend 'ctt', and no constant 'nc'.
+
+    maxlag : None or int
+        Argument for `adfuller`, largest or given number of lags.
+    autolag : str
+        Argument for `adfuller`, lag selection criterion.
+
+        * If None, then maxlag lags are used without lag search.
+        * If 'AIC' (default) or 'BIC', then the number of lags is chosen
+          to minimize the corresponding information criterion.
+        * 't-stat' based choice of maxlag.  Starts with maxlag and drops a
+          lag until the t-statistic on the last lag length is significant
+          using a 5%-sized test.
     """
-    result = coint(y_true, y_pred)
+    result = coint(y_zero, y_one)
     CointegrationResult = namedtuple('CointegrationResult', 'statistic pvalue')
     return CointegrationResult(result[0], result[1])
 
-def bds(timeseries, max_dim=2, epsilon=None, distance=1.5):
+def bds(timeseries, max_dim=10, epsilon=1.5, distance=None):
     """
     BDS documentation:
     https://www.statsmodels.org/stable/generated/statsmodels.tsa.stattools.bds.html#statsmodels.tsa.stattools.bds
 
+    Generally speaking the Brock, Dechert, and Scheinkman test
+    asks if the time series is a linear stochastic process.
     
+    Null hypothesis:
+    the time series is independent and identically distributed.
+    
+    Alternative hypothesis:
+    The timeseries is not linear.
+    
+    For more information see:
+    https://faculty.washington.edu/ezivot/econ584/notes/nonlinear.pdf
+    
+    Note: Another way to think of this test, is how chaotic is the series?
+    Since you can tune epsilon or distance.  See:
+    http://www.cs.bsu.edu/homepages/tliu/research/papers/jae1992v7ps25_liu.pdf
+    for a discussion on this and a possible selection process for epsilon.
+    
+    Parameters
+    ----------
+    timeseries : pd.Series
+        Observations of time series for which bds statistics is calculated.
+    max_dim : int
+        The maximum embedding dimension.  This number indicates the maximum
+        for how far back in the series to go when looking for linearity.
+    epsilon : float, optional
+        Specifies the distance multiplier to use when computing the test
+        statistic. 
+        Note: epsilon is deviations are within epsilon standard deviations.
+        So if epsilon is 1.5, we are checking to see if deviations, the 
+        measure of correlation are within 1.5 standard deviations.
+    distance : {float, None}, optional
+        The threshold distance to use in calculating the correlation sum.
+    
+    Returns
+    -------
+    bds_stat : float
+        The BDS statistic.
+    pvalue : float
+        The p-values associated with the BDS statistic.
     """
-    result = bds(timeseries, max_dim=max_dim, epsilon=epsilon, distance=distance)
+    result = bds(timeseries, max_dim=max_dim, epsilon=distance, distance=epsilon)
     BdsResult = namedtuple('BdsResult', 'statistic pvalue')
     return BdsResult(result[0], result[1])
 
@@ -354,35 +472,205 @@ def q_stat(timeseries):
     QstatResult = namedtuple('QstatResult', 'statistic pvalue')
     return QstatResult(result[0], result[1])
 
-def acorr_ljungbox(timeseries):
-    """
-    """
-    result = diagnostic.acorr_ljungbox(timeseries)
-    AcorrLjungBoxResult = namedtuple('AcorrLjungBoxResult', 'statistic pvalue')
-    return AcorrLjungBoxResult(result[0], result[1])
+# percentage variance/ percentage daily growth within 2 days from actual forecast
+# benchmarks against other models
+# predict two weeks out - what did our predictions say two weeks out
 
-def acorr_breusch_godfrey(timeseries):
+def acorr_breusch_godfrey(timeseries, nlags=None):
     """
+    Breusch-Godfrey Lagrange Multiplier tests for residual autocorrelation.
+    documentation can be found here:
+    https://www.statsmodels.org/stable/generated/statsmodels.stats.diagnostic.acorr_breusch_godfrey.html
+
+    This test looks for serial correlation in a timeseries.
+
+    Definition: serial correlation :=
+    Serial or auto correlation is a correlation of
+    a signal with a delayed copy of itself.  
+    
+    The metric of correlation is the Pearson correlation
+    and indicates a relationship with previous measurements
+    in the series.  The presence of serial correlation can be used
+    to understand periodicity.
+
+    See this for more details:
+    https://www.mathworks.com/help/signal/ug/find-periodicity-using-autocorrelation.html
+
+    Null hypothesis:
+    There is no serial correlation up to nlags.
+    
+    Alternative hypothesis:
+    There is serial correlation.
+    
+    Parameters
+    ----------
+    timeseries : pd.Series
+        Estimation results for which the residuals are tested for serial
+        correlation.
+    nlags : int, default None
+        Number of lags to include in the auxiliary regression. (nlags is
+        highest lag).
+        if nlags is set to None then nlags is:
+        ```
+        nlags = np.trunc(12. * np.power(nobs / 100., 1 / 4.))
+        nlags = int(nlags)
+        ```
+
+    Returns
+    -------
+    lm : float
+        Lagrange multiplier test statistic.
+    lmpval : float
+        The p-value for Lagrange multiplier test.
     """
     result = diagnostic.acorr_breusch_godfrey(timeseries)
     AcorrBreuschGodfreyResult = namedtuple('BreuschGodfreyResult', 'statistic pvalue')
     return AcorrBreuschGodfreyResult(result[0], result[1])
 
-def het_arch(timeseries):
+def het_arch(timeseries: pd.Series, nlags=None, autolag=None, ddof=0):
     """
+    Engle’s Test for Autoregressive Conditional Heteroscedasticity (ARCH).
+    
+    Null hypothesis:
+    No conditional heteroscedasticity
+    
+    Note: This does not imply no serial correlation.
+    
+    Alternative hypothesis:
+    Conditional heteroscedasticity exists
+
+    References: 
+    * https://www.mathworks.com/help/econ/archtest.html
+    * https://www.mathworks.com/help/econ/engles-arch-test.html
+
+    Definition: Heteroscedasticity :=
+
+    Heteroscedasticity means that the variance of a time series
+    is not constant over time.  Therefore the variance over sliding
+    window t,... t+i will differ from sliding window t+i+1,..t+j,
+    where t is the initial time index, i, j are integers.
+    
+    Parameters
+    ----------
+    timeseries : pd.Series
+        residuals from an estimation, or time series
+    nlags : int, default None
+        Highest lag to use. The behavior of this parameter will change
+        after 0.12.
+    autolag : {str, None}, default None
+        If None, then a fixed number of lags given by maxlag is used. This
+        parameter is deprecated and will be removed after 0.12.  Searching
+        for model specification cannot control test size.
+    ddof : int, default 0
+        If the residuals are from a regression, or ARMA estimation, then there
+        are recommendations to correct the degrees of freedom by the number
+        of parameters that have been estimated, for example ddof=p+q for an
+        ARMA(p,q).
+
+    Returns
+    -------
+    lm : float
+        Lagrange multiplier test statistic
+    lmpval : float
+        p-value for Lagrange multiplier test
     """
-    result = diagnostic.het_arch(timeseries)
+    result = diagnostic.het_arch(
+        timeseries, nlags=nlags,
+        autolag=None, ddof=ddof
+    )
     HetArchResult = namedtuple('HetArchResult', 'statistic pvalue')
     return HetArchResult(result[0], result[1])
 
-def breaks_cumsumolsresid(timeseries):
+def breaks_cumsumolsresid(timeseries, ddof=0):
     """
+    Cumulative summation test for parameter stability
+    based on ols residuals.
+    
+    documentation:
+    https://www.statsmodels.org/devel/generated/statsmodels.stats.diagnostic.breaks_cusumolsresid.html#statsmodels.stats.diagnostic.breaks_cusumolsresid
+    
+    see:
+    * https://en.wikipedia.org/wiki/Structural_break
+    
+    This test looks for 'breaks' or huge changes in the parameter of interest
+    over time, to see if there is structural instability in the series.
+    
+    Parameters
+    ----------
+    timeseries : pd.Series
+        An array of residuals from an OLS estimation.
+    ddof : int
+        The number of parameters in the OLS estimation, used as degrees
+        of freedom correction for error variance.
+
+    Returns
+    -------
+    sup_b : float
+        The test statistic, maximum of absolute value of scaled cumulative OLS
+        residuals.
+    pval : float
+        Probability of observing the data under the null hypothesis of no
+        structural change, based on asymptotic distribution which is a Brownian
+        Bridge
     """
-    result = diagnostic.breaks_cusumolsresid(timeseries)
+    result = diagnostic.breaks_cusumolsresid(timeseries, ddof=ddof)
     BreaksCumSumResult = namedtuple('BreaksCumSumResult', 'statistic pvalue')
     return BreaksCumSumResult(result[0], result[1])
 
 def chisquared_goodness_of_fit(y_true: pd.Series, y_pred: pd.Series, ddof=0, axis=0, lambda_=1):
+    """
+    The chi-squared goodness of fit test.
+    
+    Documentation:
+    https://docs.scipy.org/doc/scipy-0.14.0/reference/generated/scipy.stats.power_divergence.html
+    
+    Parameters
+    ----------
+    y_true : pd.Series
+        Observed time series.
+    y_pred : pd.Series
+        Forecasted time series.
+    ddof : int, optional
+        "Delta degrees of freedom": adjustment to the degrees of freedom
+        for the p-value.  The p-value is computed using a chi-squared
+        distribution with ``k - 1 - ddof`` degrees of freedom, where `k`
+        is the number of observed frequencies.  The default value of `ddof`
+        is 0.
+    axis : int or None, optional
+        The axis of the broadcast result of `f_obs` and `f_exp` along which to
+        apply the test.  If axis is None, all values in `f_obs` are treated
+        as a single data set.  Default is 0.
+    lambda_ : float or str, optional
+        `lambda_` gives the power in the Cressie-Read power divergence
+        statistic.  The default is 1.  For convenience, `lambda_` may be
+        assigned one of the following strings, in which case the
+        corresponding numerical value is used::
+            String              Value   Description
+            "pearson"             1     Pearson's chi-squared statistic.
+                                        In this case, the function is
+                                        equivalent to `stats.chisquare`.
+            "log-likelihood"      0     Log-likelihood ratio. Also known as
+                                        the G-test.
+            "freeman-tukey"      -1/2   Freeman-Tukey statistic.
+            "mod-log-likelihood" -1     Modified log-likelihood ratio.
+            "neyman"             -2     Neyman's statistic.
+            "cressie-read"        2/3   The power.
+    Note: it is not recommended to change the lambda_ parameter unless
+    there is very good reason, because this changes the test that is run.
+    Please only change if you would like a different test.
+
+    Returns
+    -------
+    stat : float or ndarray
+        The Cressie-Read power divergence test statistic.  The value is
+        a float if `axis` is None or if` `f_obs` and `f_exp` are 1-D.
+    p : float or ndarray
+        The p-value of the test.  The value is a float if `ddof` and the
+        return value `stat` are scalars.
+    """
     y_true = y_true[y_true > 0]
     y_pred = y_pred[y_true > 0]
-    return stats.power_divergence(y_true, y_pred)
+    return stats.power_divergence(
+        y_true, y_pred, ddof=ddof,
+        axis=axis, lambda_=lambda_
+    )
