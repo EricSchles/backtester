717ec12f65df42b492c1198650ff9185a8f5100d
Eric Schles <ericschles@gmail.com>
2020-04-09 19:11:06 -0400

    changing to index

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = 'Eric Schles'
 # The full version, including alpha/beta/rc tags
 release = '0.2'
 
-
+master_doc = 'index'
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
